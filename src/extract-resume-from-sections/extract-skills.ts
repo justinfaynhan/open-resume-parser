aababda1aee3ab34a358d1586a38a56f8f334555
Justin <justinnhan@hotmail.com>
2023-10-08 11:32:13 +1100

    init: initial porting of resume-parser logic from open-resume into ts-dx template repo

---
+++ src/extract-resume-from-sections/extract-skills.ts
@@ -0,0 +1,47 @@
+import type { FeaturedSkill, ResumeSkills } from "../resume-types";
+import type { ResumeSectionToLines } from "../types";
+import { getSectionLinesByKeywords } from "../extract-resume-from-sections/lib/get-section-lines";
+import {
+  getBulletPointsFromLines,
+  getDescriptionsLineIdx,
+} from "../extract-resume-from-sections/lib/bullet-points";
+
+const initialFeaturedSkill: FeaturedSkill = { skill: "", rating: 4 };
+const initialFeaturedSkills: FeaturedSkill[] = Array(6).fill({
+  ...initialFeaturedSkill,
+});
+
+/**
+ * Server side object deep clone util using JSON serialization.
+ * Not efficient for large objects but good enough for most use cases.
+ *
+ * Client side can simply use structuredClone.
+ */
+export const deepClone = <T extends { [key: string]: any }>(object: T) =>
+  JSON.parse(JSON.stringify(object)) as T;
+
+export const extractSkills = (sections: ResumeSectionToLines) => {
+  const lines = getSectionLinesByKeywords(sections, ["skill"]);
+  const descriptionsLineIdx = getDescriptionsLineIdx(lines) ?? 0;
+  const descriptionsLines = lines.slice(descriptionsLineIdx);
+  const descriptions = getBulletPointsFromLines(descriptionsLines);
+
+  const featuredSkills = deepClone(initialFeaturedSkills);
+  if (descriptionsLineIdx !== 0) {
+    const featuredSkillsLines = lines.slice(0, descriptionsLineIdx);
+    const featuredSkillsTextItems = featuredSkillsLines
+      .flat()
+      .filter((item) => item.text.trim())
+      .slice(0, 6);
+    for (let i = 0; i < featuredSkillsTextItems.length; i++) {
+      featuredSkills[i].skill = featuredSkillsTextItems[i].text;
+    }
+  }
+
+  const skills: ResumeSkills = {
+    featuredSkills,
+    descriptions,
+  };
+
+  return { skills };
+};
